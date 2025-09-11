ff6cdbb6b5bcace21faa0e49423d958bb9c96996
PINJAI <ananta0106bhaskara@gmail.com>
2025-09-11 13:19:29 +0700

    Added the new note popup but still not working

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,4 +1,20 @@
-let notescontainer = document.getElementById("notes-container");
-let newNoteBtn = document.getElementById("new-note-btn");
+const newNoteBtn = document.getElementById('new-note-btn');
+const popupOverlay = document.getElementById('popup-overlay');
+const cancelBtn = document.getElementById('cancel-btn');
+const noteForm = document.getElementById('note-form');
 
-let notes = JSON.parse(localStorage.getItem("notes")) || [];
+newNoteBtn.addEventListener('click', () => {
+    popupOverlay.classList.add('active');
+})
+
+cancelBtn.addEventListener('click', ()=>{
+    popupOverlay.classList.remove('active');
+    noteForm.reset();
+})
+
+popupOverlay.addEventListener('click', (e) => {
+    if (e.target === popupOverlay){
+        popupOverlay.classList.remove('active');
+        noteForm.reset();
+    }
+})
